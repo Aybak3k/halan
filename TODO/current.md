8301261b83f178d34fe14de9a1bce153e5184d02
Aybak3k <nooreldin3000@gmail.com>
2022-04-14 10:52:28 +0200

    before the storm: nav a ~ span

--- TODO/current.md
+++ TODO/current.md
@@ -1,3 +1,6 @@
+- navbar link a
+- radio box for (worker, customer)
+
 - make form styles sharable
 - add label && icons && animate label 
 - js form validation
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,23 +1,19 @@
-(function($) {
-    $(function() {
-      $('nav ul li > a:not(:only-child)').click(function(e) {
-        $(this)
-          .siblings('.nav-dropdown')
-          .slideToggle()
-        $('.nav-dropdown')
-          .not($(this).siblings())
-          .hide()
-        e.stopPropagation()
-      })
-      $('html').click(function() {
-        $('.nav-dropdown').hide()
-      })
-      // Toggle open and close nav styles on click
-      $('#burger-menu').click(function() {
-        $('nav ul').slideToggle();
-      });
-      $('#burger-menu').on('click', function() {
-        this.classList.toggle('active')
-      })
-    })
-})(jQuery)
+(function ($) {
+  $(function () {
+    // $("nav ul li > a:not(:only-child) ~ span").click(function (e) {
+    $("nav ul li > a:not(:only-child)").click(function (e) {
+      $(this).siblings(".nav-dropdown").slideToggle();
+      $(".nav-dropdown").not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    $("html").click(function () {
+      $(".nav-dropdown").hide();
+    });
+    $("#burger-menu").click(function () {
+      $("nav ul").slideToggle();
+    });
+    $("#burger-menu").on("click", function () {
+      this.classList.toggle("active");
+    });
+  });
+})(jQuery);
