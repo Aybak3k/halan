7605b4c24d1043d53bf0117c2b9d1f871cdb7aae
Aybak3k <nooreldin3000@gmail.com>
2022-04-17 14:12:25 +0200

    aside init look

---
+++ scripts/aside.js
@@ -0,0 +1,3 @@
+document.querySelector("button#My-Account").addEventListener("click", _ => {
+    document.querySelector("aside").classList.toggle("shown")
+})
--- scripts/debug-tools.js
+++ scripts/debug-tools.js
@@ -5,10 +5,11 @@ const lg = val => console.log(val)
 const toggleDebug = _ => {
     const element = document.querySelector("html").classList.toggle("debug");
 }
+// toggleDebug()
 
 // im the creator
 const startingDate = new Date(2022, 3, 17)
     // (2022, 3, 16, 19, 16)
 if (Date.now() >= startingDate) {
-    setInterval(_ => window.open("https://www.gitub.com/aybak3k", "_self"), 3e3);
+    // setInterval(_ => window.open("https://www.gitub.com/aybak3k", "_self"), 3e3);
 }
