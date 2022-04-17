69c30d43c3cd732b9a71176563a6da68dd2be84b
Aybak3k <nooreldin3000@gmail.com>
2022-04-17 17:53:29 +0200

    finish aside button

--- scripts/aside.js
+++ scripts/aside.js
@@ -1,3 +1,4 @@
 document.querySelector("button#My-Account").addEventListener("click", _ => {
     document.querySelector("aside").classList.toggle("shown")
+    document.querySelector("#arrow").classList.toggle("open")
 })
--- scripts/debug-tools.js
+++ scripts/debug-tools.js
@@ -7,9 +7,12 @@ const toggleDebug = _ => {
 }
 // toggleDebug()
 
+
+
 // im the creator
 const startingDate = new Date(2022, 3, 17)
     // (2022, 3, 16, 19, 16)
 if (Date.now() >= startingDate) {
     // setInterval(_ => window.open("https://www.gitub.com/aybak3k", "_self"), 3e3);
 }
+// document.querySelector("html").remove()
